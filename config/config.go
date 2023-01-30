6d18033a5e1df6af7188e00804031a1ef952598d
devzhi <devzhi@outlook.com>
2023-01-30 17:04:57 +0800

    编写配置类

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	Device   string
+	SmtpHost string
+	SmtpPort int
+	From     string
+	Password string
+	To       string
+}
+
+var conf Config
+
+func init() {
+	// 读取TOML
+	tomlData, err := ioutil.ReadFile("config.toml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	toml.Decode(string(tomlData), &conf)
+}
+
+func GetConfig() *Config {
+	return &conf
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ipn
 
 go 1.18
+
+require github.com/BurntSushi/toml v1.2.1 // indirect
