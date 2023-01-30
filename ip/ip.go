969868953dac9e7986f8f30f2ea70e5ce8f7967e
devzhi <devzhi@outlook.com>
2023-01-30 15:34:36 +0800

    编写读取旧IP方法

--- ip/ip.go
+++ ip/ip.go
@@ -2,7 +2,9 @@ package ip
 
 import (
 	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 /*
@@ -18,5 +20,36 @@ func GetIPInfo() (*string, error) {
 		return nil, err
 	}
 	s := string(body)
+	return &s, nil
+}
+
+/*
+读取旧IP方法
+*/
+func ReadOldIP() (*string, error) {
+	f, err := os.Open("old_ip.txt")
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	filesize := fi.Size()
+	buffer := make([]byte, filesize)
+
+	bytesread, err := f.Read(buffer)
+	if err != nil {
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	s := string(bytesread)
+
 	return &s, nil
 }
